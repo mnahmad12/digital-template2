f163a7e5b09280836d2604fd44296ecb3b7e2d3f
Mohammad Ahmad <mnahmad12@gmail.com>
2015-02-12 09:13:57 -0500

    x

--- js/main.js
+++ js/main.js
@@ -21,6 +21,8 @@ function create() {
     game.physics.startSystem(Phaser.Physics.P2JS);
 
 	box = game.add.sprite(game.world.centerX, game.world.centerY, 'box');
+	box.x=100;
+	box.y=100;
     player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
 
     game.physics.p2.enable(player);
