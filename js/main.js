1a904a5a4e6028748d9d84bec55f4e54f730c4e7
Mohammad Ahmad <mnahmad12@gmail.com>
2015-02-12 08:48:06 -0500

    g

--- js/main.js
+++ js/main.js
@@ -1,330 +1,71 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-    
-	
-	var P2Game = {};
-	
-	P2Game.StateA = function (game) {
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
-		this.message;
-		this.cursors;
-		this.logo;
-		this.background;
-		this.text;
-		this.style;
-	};
-	
-	P2Game.StateA.prototype = 
-	{
-		
-		
-		 gotoStateB: function () 
-		{
+function preload() {
 
-			this.state.start('StateB');
+    game.load.image('background','assets/wood.png');
+    game.load.image('player','assets/phaser-dude.png');
 
-		},
-		
-		preload: function()
-		{
-			this.load.image('worldBack','assets/maze.png')
-			
-		},
-		
-		update: function()
-		{
-			this.background=this.game.add.tileSprite(0, 0,800,600, 'worldBack');
-			
-			this.style = {font: "25px Arial", fill: "#FF0000", align: "center" };
-			this.text = this.add.text( this.world.centerX, 150, "Get through the maze alive and catch the cat!", this.style );
-			this.text.anchor.setTo( 0.5, 0.0 );
-			
-			
-			this.cursors = this.input.keyboard.createCursorKeys();
-			if (this.cursors.left.isDown)
-			{
-				this.gotoStateB();
-			}
-		}
-	
-	}
-	
-	
-	P2Game.StateD=function(game){
-		this.cursors;
-		this.style;
-		this.text;
-	}
-	
-	P2Game.StateD.prototype=
-	{
-		gotoStateA: function () 
-		{
+}
 
-			this.state.start('StateA');
+var player;
+var cursors;
 
-		},
-		
-		update: function()
-		{
-			
-			this.cursors = this.input.keyboard.createCursorKeys();
-			
-			this.style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-			this.text = this.add.text( this.world.centerX, 15, "You Win!\nTo play the next level please\nSend your credit card info to mahmad15@gmu.edu\n(To Restart press the left arrow)", this.style );
-			this.text.anchor.setTo( 0.5, 0.0 );
-			
-			if (this.cursors.left.isDown)
-			{
-				this.gotoStateA();
-			}
-			
-			
-		}
-	
-	}
-	
-	P2Game.StateC = function (game) {
+function create() {
 
-		this.message;
-		this.cursors;
-		this.logo;
-		this.text;
-		this.style;
-		
-	};
-	
-	P2Game.StateC.prototype = 
-	{
-		
-		
-		 gotoStateB: function () 
-		{
+    game.add.tileSprite(0, 0, 1920, 1920, 'background');
 
-			this.state.start('StateB');
+    game.world.setBounds(0, 0, 1920, 1920);
 
-		},
-		
-		
-	
-		
-		update: function()
-		{
-			
-			this.cursors = this.input.keyboard.createCursorKeys();
-			
-			this.style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-			this.text = this.add.text( this.world.centerX, 15, "You Lose! Want to Play Again? Press The Left Arrow!", this.style );
-			this.text.anchor.setTo( 0.5, 0.0 );
-			
-			if (this.cursors.left.isDown)
-			{
-				this.gotoStateB();
-			}
-		
-		}
-	
-	}
-    
-	
-	P2Game.StateB = function(game)
-		{
-			this.mummies;
-			this.bob;
-			this.keys;
-			this.text;
-			this.style;
-			this.key;
-		}
-	
-	P2Game.StateB.prototype=
-	{
-		preload: function() 
-		{
-        //pre-loading the zombies
-			this.load.spritesheet('bob', 'assets/phaser-dude.png');
-			this.load.spritesheet('mummy', 'assets/metalslug_monster39x40.png',39,40,16);
-			this.load.spritesheet('key','assets/key.png',32,24);
-			this.game.stage.backgroundColor = '#000000';
-		},
-    
+    game.physics.startSystem(Phaser.Physics.P2JS);
 
-	
-		create: function() 
-		{
-			
-		//world boundary
-			this.world.setBounds(0,0,800,600);
-			
-		//starting physics:
-			this.game.physics.startSystem(Phaser.Physics.ARCADE);
-		
-		//creating mummies
-			this.mummies = this.add.group();
-		//and bob
-			this.bob = this.add.sprite(300,100,'bob');
-		//adding the key
-			this.key=this.add.sprite(600,200,'key');
-			this.key.exists=false;
-			
-		
-		//Creating 10 mummies, each initially dead
-			this.mummies.createMultiple(50,"mummy",0,false);
-		
-			this.game.physics.enable(this.key,Phaser.Physics.ARCADE);
-			this.game.physics.enable(this.mummies,Phaser.Physics.ARCADE);
-			this.game.physics.enable(this.bob,Phaser.Physics.ARCADE);
-		
-		//bringing mummies to life!
-			
-			this.game.time.events.repeat(Phaser.Timer.SECOND/2, 100, this.resurrect,this );
-			this.game.time.events.repeat(Phaser.Timer.SECOND,100,this.randKey,this)
- 
-        //getting the key to move around and bounce
-			this.key.body.collideWorldBounds=true;
-			this.key.body.velocity.setTo(200,200);
-			
-			this.physics.arcade.collide(this.mummies,this.key);
-			this.key.body.bounce.set(1);
-	
-	
-			// Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-			this.style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-			this.text = this.add.text( this.world.centerX, 15, "Don't Let the Monsters Get You!.", this.style );
-			this.text.anchor.setTo( 0.5, 0.0 );
-		
-		
-			this.keys = this.input.keyboard.createCursorKeys();
-		
-			
-		},
-	
-		randKey: function(){
-			
-			if(!this.key.exists)
-			{
-				this.key.exists=true;
-			}
-			
-			this.key.x=this.rnd.integerInRange(0,800);
-			this.key.y=this.rnd.integerInRange(0,600);
-			
-		},
-	
-	
-		resurrect: function()
-		{
-			
-		
-			
-		 //Get a dead item
-			var item = this.mummies.getFirstDead();
-			
-			if (item)
-			{
-			 //And bring it back to life
-				item.reset((this.world.randomX + (this.key.x)), (this.world.randomY+(this.key.y)));
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
 
-			 //This just changes its frame
-				item.frame = this.rnd.integerInRange(0, 36);
-			
-			//getting the mummies to run
-				item.animations.add('walk');
+    game.physics.p2.enable(player);
 
-				item.animations.play('walk', 30, true);
-			
-			//and move towards cursor
-				item.rotation = this.physics.arcade.moveToObject( item,this.bob, 200+this.rnd.integerInRange(10,200) );
-				item.checkWorldBounds=true;
-			}
+    cursors = game.input.keyboard.createCursorKeys();
 
-			
-		},
-	
-		update: function()
-		{
-			//window.alert("here?");
-		
-		
-		
-			
-				
-			//inital bob speed
-			this.bob.body.velocity.x=0;
-			this.bob.body.velocity.y=0;
-			
-			if (this.keys.left.isDown)
-			{
-				this.bob.body.velocity.x=-200;	
-			}
-		
-			if (this.keys.right.isDown)
-			{
-				this.bob.body.velocity.x=200;	
-			}
-		
-			if (this.keys.up.isDown)
-			{	
-				this.bob.body.velocity.y=-200;	
-			}
-		
-			if (this.keys.down.isDown)
-			{	
-				this.bob.body.velocity.y=200;
-			}
-	
-			if(this.physics.arcade.overlap(this.bob,this.mummies,null,null,this))
-			{
-				this.bob.kill();
-				this.bob.exists=false;	
-				this.goToStateC();
-			}
-			
-			if(this.physics.arcade.overlap(this.bob,this.key,null,null,this))
-			{
-				this.goToStateD();
-			}
-			
-			
-				
-			
-		
-		},
-		
-		goToStateC: function () 
-		{
+    game.camera.follow(player);
 
-			this.state.start('StateC');
+    //  The deadzone is a Rectangle that defines the limits at which the camera will start to scroll
+    //  It does NOT keep the target sprite within the rectangle, all it does is control the boundary
+    //  at which the camera will start to move. So when the sprite hits the edge, the camera scrolls
+    //  (until it reaches an edge of the world)
+    game.camera.deadzone = new Phaser.Rectangle(100, 100, 600, 400);
 
-		},
-		
-		goToStateD: function () 
-		{
+}
 
-			this.state.start('StateD');
+function update() {
 
-		}
-	}
-	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
-    
-	game.state.add('StateA', P2Game.StateA);
-	game.state.add('StateB', P2Game.StateB);
-	game.state.add('StateC', P2Game.StateC);
-	game.state.add('StateD', P2Game.StateD);
-	game.state.start('StateA');
-    
-    
- };
+    player.body.setZeroVelocity();
+
+    if (cursors.up.isDown)
+    {
+        player.body.moveUp(300)
+    }
+    else if (cursors.down.isDown)
+    {
+        player.body.moveDown(300);
+    }
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -300;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.moveRight(300);
+    }
+
+}
+
+function render() {
+
+    var zone = game.camera.deadzone;
+
+    game.context.fillStyle = 'rgba(255,0,0,0.6)';
+    game.context.fillRect(zone.x, zone.y, zone.width, zone.height);
+
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player, 32, 500);
+
+}
