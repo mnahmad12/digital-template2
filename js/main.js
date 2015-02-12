009af38300cbb35c772e9449abe74eef489f1f63
Mohammad Ahmad <mnahmad12@gmail.com>
2015-02-12 08:51:21 -0500

    no red

--- js/main.js
+++ js/main.js
@@ -62,8 +62,7 @@ function render() {
 
     var zone = game.camera.deadzone;
 
-    game.context.fillStyle = 'rgba(255,0,0,0.6)';
-    game.context.fillRect(zone.x, zone.y, zone.width, zone.height);
+    
 
     game.debug.cameraInfo(game.camera, 32, 32);
     game.debug.spriteCoords(player, 32, 500);
